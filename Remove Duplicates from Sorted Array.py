1d675269c74baf2c1747d009b82b05f0bf23f200
razen1234 <133649123+razen1234@users.noreply.github.com>
2025-11-13 21:05:02 +0200

    RemoveDuplicates method
    Implement a solution to remove duplicates from a sorted array.

---
+++ Remove Duplicates from Sorted Array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # Creating two variable as pointers to count duplicates and numbers witnout them
+        no_dupl = 0 
+        dupl = 1
+        # Cycle "while" will only work in array "nums" 
+        while dupl in range(len(nums)):
+            # If numbers are same count it in "dupl"
+            if nums[no_dupl] == nums[dupl]:
+                dupl += 1
+            else:
+                # If they're not same, write it in "no_dupl"
+                nums[no_dupl+1] = nums[dupl]
+                # Move forward both variable
+                no_dupl += 1
+                dupl += 1
+        # return answer(+1 because indexing)
+        return no_dupl + 1 
